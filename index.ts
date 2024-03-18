66810dbebd81add7f39b57b296830ddd7041b5ad
Ibbad-Shabbir <ibbadshabbir02@gmail.com>
2024-03-19 01:24:18 +0500

    calculator done

---
+++ index.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter first number",
+    type: "number",
+    name: "fn",
+  },
+  {
+    message: "Enter second number",
+    type: "number",
+    name: "sn",
+  },
+  {
+    message: "choose operator",
+    type: "list",
+    name: "operator",
+    choices: ["Add", "Sub", "Mul", "Div"],
+  },
+]);
+
+if (answer.operator === "Add") {
+    console.log(answer.fn + " + " + answer.sn + " = " + (answer.fn + answer.sn));
+} else if (answer.operator === "Sub") {
+    console.log(answer.fn + " - " + answer.sn + " = " + (answer.fn - answer.sn));
+} else if (answer.operator === "Mul") {
+    console.log(answer.fn + " x " + answer.sn + " = " + (answer.fn * answer.sn));
+} else if (answer.operator === "Div") {
+    console.log(answer.fn + " / " + answer.sn + " = " + (answer.fn / answer.sn));
+}
